135fe06a9b78e9520d41195d675e008bc6791290
Djoni Bourscheid <djonibourscheid00@gmail.com>
2020-10-14 13:33:01 -0300

    rename Orphanage.ts

--- backend/src/models/Orphanage.ts
+++ backend/src/models/Orphanage.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany, JoinColumn } from 'typeorm';
 
 @Entity('orphanages')
 export default class Orphanages {
