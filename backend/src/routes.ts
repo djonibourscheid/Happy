eb002cc2e30212f6bfe779f2ea4340d7c4cb402a
Djoni Bourscheid <djonibourscheid00@gmail.com>
2020-10-14 13:01:39 -0300

    configurations for uploads of images

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
 import OrphanagesController from './controllers/OrphanagesController';
 
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
 // ROTAS
 routes.get('/orfanatos', OrphanagesController.index);
 routes.get('/orfanatos/:id', OrphanagesController.show);
-routes.post('/orfanatos', OrphanagesController.create);
+routes.post('/orfanatos', upload.array('images'), OrphanagesController.create);
 
 export default routes;
